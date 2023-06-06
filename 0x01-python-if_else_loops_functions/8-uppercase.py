8755cb5d48e2682750a0d5c6d176ee53e553a410
Collins Ogera <ogeracollins@gmail.com>
2023-06-06 20:43:33 +0000

    Indentation

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@ def uppercase(str):
     strA = list()
     for i in str:
         if ord(i) >= 97 and ord(i) <= 122:
-           strA.append(chr(ord(i) - 32))
+            strA.append(chr(ord(i) - 32))
         else:
             strA.append(i)
     print('{}'.format("".join(strA)), end="")
