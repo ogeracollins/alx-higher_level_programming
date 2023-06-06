3e3e4accf4e887e009037597e920944feb47a284
Collins Ogera <ogeracollins@gmail.com>
2023-06-06 20:40:10 +0000

    Uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    strA = list()
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+           strA.append(chr(ord(i) - 32))
+        else:
+            strA.append(i)
+    print('{}'.format("".join(strA)), end="")
+    print()
