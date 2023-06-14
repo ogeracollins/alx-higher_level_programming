2d14433019b6c51a85c900cf7ffbfab4b978a921
Collins Ogera <ogeracollins@gmail.com>
2023-06-14 22:34:19 +0000

    Infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import sys
 
-length = len(sys.argv)
-sum = 0
-
-if length > 1:
-    for i in range(1, length):
-        sum += int(sys.argv[i])
-print(sum)
+if __name__ == "__main__":
+    length = len(sys.argv)
+    sum = 0
+    if length > 1:
+        for i in range(1, length):
+            sum += int(sys.argv[i])
+    print(sum)
