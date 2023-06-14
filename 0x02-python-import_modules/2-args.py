e7bd0be5f5575a7256a59ec7df4a4580e21a935a
Collins Ogera <ogeracollins@gmail.com>
2023-06-14 22:32:57 +0000

    Arguements

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-import sys
 
-length = len(sys.argv)
-if length == 2:
-    print("{}: argument".format(length - 1))
-else:
-    print("{}: arguments".format(length - 1))
-for i in range(1, length):
-    print("{}: {}".format(1, sys.argv[i]))
+if __name__ == "__main__":
+    import sys
+
+    length = len(sys.argv)
+    if length == 2:
+        print("{}: argument".format(length - 1))
+    else:
+        print("{}: arguments".format(length - 1))
+    for i in range(1, length):
+        print("{}: {}".format(1, sys.argv[i]))
