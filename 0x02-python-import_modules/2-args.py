126487df91929c02a6086c1a4d424d5e6b0ceff6
Collins Ogera <ogeracollins@gmail.com>
2023-07-18 09:07:40 +0000

    D.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     length = len(sys.argv) - 1
 
     if length == 0:
-        print("{} arguments".format(length))
+        print("{} arguments.".format(length))
     else:
         if length == 1:
             print("{} argument:".format(length))
