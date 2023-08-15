a2ae2234f1aba22f63ad9e7bb5178a797f604bac
Collins Ogera <ogeracollins@gmail.com>
2023-08-15 10:45:27 -0700

    Group

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- List records with same score
-SELECT COUNT(score) as score, score as number FROM second_table GROUP BY score DESC;
+SELECT score, COUNT(score) as number FROM second_table GROUP BY score ORDER BY number DESC;
