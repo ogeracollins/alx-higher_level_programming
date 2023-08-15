a5f0c6fa73194545129348b3830d6215df018672
Collins Ogera <ogeracollins@gmail.com>
2023-08-15 10:43:09 -0700

    Asc to Desc

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- List records by best
-SELECT * FROM second_table ORDER BY score ASC;
+SELECT score, name FROM second_table ORDER BY score DESC;
--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,6 @@
 -- Creates a second table and insert data into it
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+
 INSERT INTO second_table (id, name, score) VALUES (id=1, name='John', score=10);
 INSERT INTO second_table (id, name, score) VALUES (id=2, name='Alex', score=3);
 INSERT INTO second_table (id, name, score) VALUES (id=3, name='Bob', score=14);
