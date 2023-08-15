b2959ab06123b97a9291fc9dd1658972b511f54d
Collins Ogera <ogeracollins@gmail.com>
2023-08-15 10:41:07 -0700

    Individual insertion

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,11 +1,6 @@
 -- Creates a second table and insert data into it
-CREATE TABLE IF NOT EXISTS second_table (
-	id INT,
-	name VARCHAR(256),
-	score INT
-);
-INSERT INTO second_table (id, name, score) VALUES 
-	(id=1, name='John', score=10),
-	(id=2, name='Alex', score=3),
-	(id=3, name='Bob', score=14),
-	(id=4, name='George', score=8);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score) VALUES (id=1, name='John', score=10);
+INSERT INTO second_table (id, name, score) VALUES (id=2, name='Alex', score=3);
+INSERT INTO second_table (id, name, score) VALUES (id=3, name='Bob', score=14);
+INSERT INTO second_table (id, name, score) VALUES (id=4, name='George', score=8);
