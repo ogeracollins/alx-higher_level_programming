9e8072e3208bbf6f8e5a6b6c99b00f866e3c6e5a
Collins Ogera <ogeracollins@gmail.com>
2023-08-16 01:43:51 -0700

    Cities table

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+-- Creates database hbtn_0d_usa
+-- create table cities
+-- cities has attr id, state_id, name
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+use hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+	id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	FOREIGN KEY(state_id) REFERENCES states(id)
+)engine=InnoDB;
