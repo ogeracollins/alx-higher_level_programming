ed34bcbb19e2b68212314ccd641a8338f1492afb
Collins Ogera <ogeracollins@gmail.com>
2023-08-16 13:22:09 -0700

    Only Comedy hahaaaaa

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -4,8 +4,6 @@ JOIN tv_show_genres
 ON tv_shows.id=tv_show_genres.show_id
 JOIN tv_genres
 ON tv_genres.id=tv_show_genres.genre_id
-WHERE tv_show_genres.genre_id = (
-	SELECT id FROM tv_genres
-	WHERE name="Comedy")
+WHERE tv_genres.name="Comedy"
 ORDER BY title ASC;
 
