56ec7cb01f9b55664b62e5eefeb5421129607c79
Collins Ogera <ogeracollins@gmail.com>
2023-08-16 10:50:21 -0700

    Naming convention

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,5 @@
 -- List all cities contained in the database
-use hbtn_0d_usa;
-SELECT cities.id as city_id, cities.name as city_name, states.name as state_name
+SELECT cities.id as city_id, cities.name as name, states.name as name
 FROM cities
 JOIN states
 ON states.id = cities.state_id
