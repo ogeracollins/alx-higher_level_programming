693580d51d80d3fccc5ed1ddc4c013de23f83ce0
Collins Ogera <ogeracollins@gmail.com>
2023-08-16 13:55:42 -0700

    Not in my genre

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,9 @@
+-- List all genres not linked to the show
+SELECT name FROM tv_genres
+WHERE id NOT IN (
+	SELECT genre_id
+	FROM tv_show_genres
+	JOIN tv_shows ON tv_show_genres.show_id=tv_shows.id
+	WHERE tv_shows.title='Dexter'
+)
+ORDER BY name ASC;
