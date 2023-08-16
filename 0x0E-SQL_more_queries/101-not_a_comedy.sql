fbf09807de4181d2e060ad5d48b8bcb4ae8644db
Collins Ogera <ogeracollins@gmail.com>
2023-08-16 14:00:51 -0700

    No comedy tonight

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,8 @@
+-- List all shows without genre comedy
+SELECT title FROM tv_shows
+WHERE id NOT IN (
+	SELECT show_id FROM tv_show_genres
+	JOIN tv_genres ON tv_show_genres.genre_id=tv_genres.id
+	WHERE tv_genres.name='Comedy'
+)
+ORDER BY title ASC;
