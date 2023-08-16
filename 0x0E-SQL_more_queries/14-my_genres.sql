2319a598e1f3aa2bae44d6133027b2df0d7acd54
Collins Ogera <ogeracollins@gmail.com>
2023-08-16 13:23:19 -0700

    My genres

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -4,7 +4,5 @@ JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows
 ON tv_shows.id = tv_show_genres.show_id
-WHERE show_id = (
-	SELECT id FROM tv_shows WHERE title='Dexter'
-)
+WHERE tv_shows.title="Dexter"
 ORDER BY name ASC;
