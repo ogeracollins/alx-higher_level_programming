01ef962468eb73a1d1f0cf470d7c56f300083d4e
Collins Ogera <ogeracollins@gmail.com>
2023-08-16 12:34:10 -0700

    Cities by states

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,7 @@
 -- List all cities contained in the database
-SELECT cities.id as city_id, cities.name as name, states.name as name
+SELECT cities.id, cities.name, states.name
 FROM cities
 JOIN states
 ON states.id = cities.state_id
-ORDER BY city_id;
+ORDER BY cities.id;
 
