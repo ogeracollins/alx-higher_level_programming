5700dbd8f2afcc6bdaa2008642e2e1886be96d4e
Collins Ogera <ogeracollins@gmail.com>
2023-09-12 21:43:51 +0000

    Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const langLength = languages.length;
+
+for (let i = 0; i < langLength; i++) {
+  console.log(languages[i]);
+}
