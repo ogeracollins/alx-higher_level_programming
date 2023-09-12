2b7f056e71996be90fcbf2a09d76f605f14bec24
Collins Ogera <ogeracollins@gmail.com>
2023-09-12 21:29:16 +0000

    Argument value

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const process = require('process');
 const args = process.argv;
-const argsLength = args.length - 2;
 
-if (argsLength === 0) {
+if (args[2] === undefined) {
   console.log('No argument');
 } else {
   console.log(`${args[2]}`);
