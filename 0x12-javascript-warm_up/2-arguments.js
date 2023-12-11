08eb3efdaac3408477e5603225c9f51a2eefa50b
Collins Ogera <ogeracollins@gmail.com>
2023-12-11 15:39:23 -0800

    resolve

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 //Print number of arguments passed
 
-let arguments = process.argv.length - 2;
-if (arguments == 0){
+if (process.argv.length == 2){
     console.log("No argument");
-} else if (arguments == 1){
+} else if (process.argv.length == 3){
     console.log("Argument found");
 } else {
     console.log("Arguments found");
